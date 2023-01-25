12addc392b979bbdd3e924464833a6f682ea4947
Anezeres <alvarez.julian@correounivalle.edu.co>
2023-01-25 07:29:10 -0500

    Se organiza el tema de las rutas y se inicia la parte de vistas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+const home = require('../controllers/home');
+const image = require('../controllers/image');
+
+
 module.exports = app => {
 
-    app.get('/', (req, res) => {
-        res.send('Index page');
-    });
+    router.get('/', home.index);
+    router.get('/trabajador/:id', image.index);
+    router.post('/trabajador', image.create);
+    router.post('/trabajador/:id/like', image.like);
+    router.post('/trabajador/:id/comment', image.comment);
+    router.delete('/trabajador/:id', image.remove);
+
+
+    app.use(router);
 }
